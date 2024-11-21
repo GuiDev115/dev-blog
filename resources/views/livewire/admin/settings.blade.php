92b3e72ff61d285affeb08b6561afe178213a496
guidev <guilherme.fabricio.115@gmail.com>
2024-11-20 23:11:49 -0300

    :sparkles: feat: added the functionality to update the site's general settings to Livewire's Settings class

--- resources/views/livewire/admin/settings.blade.php
+++ resources/views/livewire/admin/settings.blade.php
@@ -11,7 +11,54 @@
         <div class="tab-content">
             <div class="tab-pane fade {{ $tab == 'general_settings' ? 'active show' : '' }}" id="general_settings" role="tabpanel">
                 <div class="pd-20">
-                    --- Config geral ---
+                    <form wire:submit="updateSiteInfo()">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for=""><b>Nome do Site</b></label>
+                                    <input type="text" class="form-control" wire:model="site_title" placeholder="Entre com seu Nome do Site">
+                                    @error('site_title')
+                                        <span class="text-danger ml-1">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for=""><b>Email do Site</b></label>
+                                    <input type="text" class="form-control" wire:model="site_email" placeholder="Entre com seu Email do Site">
+                                    @error('site_email')
+                                        <span class="text-danger ml-1">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for=""><b>Telefone do Site</b><small> (Optional) </small></label>
+                                    <input type="text" class="form-control" wire:model="site_phone" placeholder="Entre com seu Telefone do Site">
+                                    @error('phone_title')
+                                        <span class="text-danger ml-1">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for=""><b>Meta do Site</b><small> (Optional) </small></label>
+                                    <input type="text" class="form-control" wire:model="site_title" placeholder="Entre com seu Meta Keywords do Site">
+                                    @error('site_meta_keywords')
+                                        <span class="text-danger ml-1">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for=""><b>Site Meta Descricao</b><small> (Optional)</small></label>
+                            <textarea class="form-control" cols="4" rows="4" placeholder="Digita a descricao do site"></textarea>
+                            @error('site_meta_description')
+                                <span class="text-danger ml-1">{{ $message }}</span>
+                            @enderror
+                        </div>
+                        <button type="submit" class="btn btn-primary">Atualizar</button>
+                    </form>
                 </div>
             </div>
             <div class="tab-pane fade {{ $tab == 'logo_favicon' ? 'active show' : '' }}" id="logo_favicon" role="tabpanel">
