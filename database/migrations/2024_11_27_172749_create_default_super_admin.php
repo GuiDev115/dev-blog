279fabb80923c6a13ea682e3bc5e0dfd13d7bed5
guidev <guilherme.fabricio.115@gmail.com>
2024-11-27 14:52:08 -0300

    :wrench: fix: create user default with migrate

---
+++ database/migrations/2024_11_27_172749_create_default_super_admin.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        DB::table('users')->insert([
+            'id' => 1,
+            'name' => 'Super Admin',
+            'username' => 'admin',
+            'email' => 'admin@devblog.com',
+            'password' => bcrypt('admin'),
+            'picture' => 'default-avatar.jpg',
+            'bio' => 'Conta Padrao com Super Admin',
+            'type' => 'superAdmin',
+            'status' => 'active',
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('default_super_admin');
+    }
+};
